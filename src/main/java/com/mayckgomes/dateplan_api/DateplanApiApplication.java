96b7816897c1408709c56c7197a0167bf1bb3d3d
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-05-07 15:16:45 -0300

    Initial commit

---
+++ src/main/java/com/mayckgomes/dateplan_api/DateplanApiApplication.java
@@ -0,0 +1,13 @@
+package com.mayckgomes.dateplan_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateplanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateplanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mayckgomes/dateplan_api/DateplanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayckgomes.dateplan_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateplanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
