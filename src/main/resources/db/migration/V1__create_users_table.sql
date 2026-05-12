a441cd669231e52482460e212220bad1db60850f
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-05-12 12:57:44 -0300

    refactor: renamed columns using _

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users(
     id BIGSERIAL PRIMARY KEY,
-    public_Id VARCHAR(50) NOT NULL,
+    public_id VARCHAR(50) NOT NULL,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     password VARCHAR(250) NOT NULL,
-    relationship_Id BIGINT,
+    relationship_id BIGINT,
     plan VARCHAR(50) NOT NULL,
-    notification_Token VARCHAR(250) NOT NULL
+    notification_token VARCHAR(250) NOT NULL
 );
--- src/main/resources/db/migration/V2__create_relationship_table.sql
+++ src/main/resources/db/migration/V2__create_relationship_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE relationship(
     id BIGSERIAL PRIMARY KEY,
-    idUser1 BIGINT NOT NULL,
-    idUser2 BIGINT NOT NULL,
-    initialDay VARCHAR(50) NOT NULL
+    id_user1 BIGINT NOT NULL,
+    id_user2 BIGINT NOT NULL,
+    initial_day VARCHAR(50) NOT NULL
 );
