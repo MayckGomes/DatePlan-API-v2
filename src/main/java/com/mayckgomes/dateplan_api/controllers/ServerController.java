ca9e3fc3530c6d6c9da5c615db4bd5193860b23b
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-05-21 15:46:55 -0300

    feat: added server controller for healty checks

---
+++ src/main/java/com/mayckgomes/dateplan_api/controllers/ServerController.java
@@ -0,0 +1,18 @@
+package com.mayckgomes.dateplan_api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ServerController {
+
+    @GetMapping
+    public ResponseEntity<Void> checkStatus(){
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+}
