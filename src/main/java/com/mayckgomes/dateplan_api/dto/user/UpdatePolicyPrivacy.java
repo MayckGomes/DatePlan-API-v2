f2f3004ad7df371fb31bdacfdc4f66afb1e5e613
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-06-11 12:04:20 -0300

    fix: fix validation

--- src/main/java/com/mayckgomes/dateplan_api/dto/user/UpdatePolicyPrivacy.java
+++ src/main/java/com/mayckgomes/dateplan_api/dto/user/UpdatePolicyPrivacy.java
@@ -12,7 +12,6 @@ import lombok.Setter;
 public class UpdatePolicyPrivacy {
 
     @NotNull
-    @NotBlank
     private Long version;
 
     @NotNull
--- src/main/java/com/mayckgomes/dateplan_api/dto/user/UpdateTermsOfUse.java
+++ src/main/java/com/mayckgomes/dateplan_api/dto/user/UpdateTermsOfUse.java
@@ -12,7 +12,6 @@ import lombok.Setter;
 public class UpdateTermsOfUse {
 
     @NotNull
-    @NotBlank
     private Long version;
 
     @NotNull
